9c47b6f2d547ca15c86c56ac25f8f92f0350eed1
adeilson2 <adeilsonqualidade@gmail.com>
2019-01-21 22:27:36 -0200

    Pedido, estadoPagamento, Pagamento

---
+++ src/main/java/com/alves/cursomc/domain/PagamentoComCartao.java
@@ -0,0 +1,35 @@
+package com.alves.cursomc.domain;
+
+import javax.persistence.Entity;
+
+import com.alves.cursomc.domain.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComCartao extends Pagamento{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer numeroDeParcelas;
+	
+	PagamentoComCartao (){
+		
+	}
+
+	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
+		super(id, estado, pedido);
+		
+		this.numeroDeParcelas = numeroDeParcelas;
+		
+	}
+
+	public Integer getNumeroDeParcelas() {
+		return numeroDeParcelas;
+	}
+
+	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+	
+	
+	
+
+}
---
+++ src/main/java/com/alves/cursomc/services/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.alves.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alves.cursomc.domain.Categoria;
+import com.alves.cursomc.repositories.CategoriaRepository;
+import com.alves.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		}
+}
